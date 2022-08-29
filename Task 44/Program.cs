84684004e5b83d2abf2cfc08f6e31a648b454f41
unknown <bdn2019962@yandex.ru>
2022-08-29 18:04:42 +0400

    Web 6

---
+++ Task 44/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] fibonacci = new int[N];
+int a = 0;
+int b = 1;
+fibonacci[0] = a;
+fibonacci[1] = b;
+for(int i = 2; i < N ; i++)
+{
+    int c = a + b;
+    fibonacci[i] = c;
+    a = b;
+    b = c;
+}
+foreach(int cur in fibonacci)
+{
+    Console.WriteLine(cur);
+}
---
+++ Task 45/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите количество элементов в массиве");
+int Count = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[Count];
+Console.WriteLine("Введите числа массива");
+for(int i = 0; i < array.Length; i++)
+{
+   array[i] = Convert.ToInt32(Console.ReadLine());
+}
+int[] copy = new int[array.Length];
+Array.Copy(array, copy, array.Length);
+Console.WriteLine(String.Join(", ", copy));
